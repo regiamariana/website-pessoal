619f685b13b895a9957222907e91daf4d62786a1
mariana regia <guirado.mariana03@gmail.com>
2022-01-25 23:36:49 -0300

    up

--- website-mariana/src/pages/home.js
+++ website-mariana/src/pages/home.js
@@ -2,16 +2,25 @@
 // import React from 'react';
 // import { Link } from "react-router-dom";
 import React from "react";
+import { Link } from "react-router-dom";
 import '../pages/home.css'
 function Mari() {
     return(
         <section className="general">
-            <header>
-                
-            </header>
 
             <body>
                 
+            <header>
+                <nav>
+                    <ul>
+                        <li><Link to="/teste">Home</Link></li>
+                        <li><Link href="#">O que eu faço</Link></li>
+                        <li><Link href="#">Skills</Link></li>
+                        <li><Link href="">Sobre mim</Link></li>
+                        <li><Link href="">Contato</Link></li>
+                    </ul>
+                </nav>
+            </header>
             </body> 
 
             <footer></footer>      
